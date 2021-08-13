031e76bd9da53c09971a78f0c6f67aa0e6442858
alokeshdatta <57950476+alokeshdatta@users.noreply.github.com>
2021-08-13 02:49:35 -0500

    Update GithubRestProject.java

--- API/Project/GithubRestProject.java
+++ API/Project/GithubRestProject.java
@@ -24,7 +24,7 @@ public class GithubRestProject {
 	public void setUp() {
 		  
 		  requestSpec = new RequestSpecBuilder().setContentType(ContentType.JSON)
-				  		.addHeader("Authorization","token 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")
+				  		.addHeader("Authorization","token ******")
 				        .setBaseUri(baseURI)
 				        .build();
 	
@@ -33,7 +33,7 @@ public class GithubRestProject {
   @Test(priority=1)
   public void postRequest() {
 	  
-	  String requestBody = "{\"title\": \"TestAPIKey\", \"key\": \"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6bfyTH4ByJkYhvsjk4dJU9dqCZPxpwTTbINAr3SxR6dP3ClURFHiGzJMdQImgEFpUYAlljMiNqdBXTPd/NOITHbDL1k2lZctPhctT39FgvqGB6AWEPryVkDZQBeNzvh+VDi9mIEKjbiLnzpf3opM6E6H+7f81EOCKSFXb8hn3YysStuyzdy51l4tx/sjZtwgZY+N5i2ctAghhyuyABZ2RuY6rqLe/Ubpdr4YK8+e7Y3bmAthbu0RhVgr2tpqxKxO8Z80mvbX5TUHJNg3zOJfkjxJGggEdBRjkVLV3dSBoIzSl8HLaS4ZCKkxuNCITcRAkiCxqv5O8E9szMwfWN0BxOb9JNjIHBMFoXQrFafSOf9Rc+EXeSHLcNLioY7KYbCg4EaWz5d87JbJyJqvhD9iLDROtMQyNXg9LdqhW1scRWf6VvRcJRJhZE7OfWBe3RKVKW1LNfR0Er8IqAb87v2Rh2MxvPfF16GF6X9g2lWSD5Lojta7o86qL+bZWuRvTVQM=\", \"read_only\": \"true\"}";
+	  String requestBody = "{\"title\": \"TestAPIKey\", \"key\": \"ssh-rsa *****\", \"read_only\": \"true\"}";
       Response response = given().spec(requestSpec) 
               .body(requestBody) 
               .when().post("/user/keys").then().extract().response();
